d8991bf584d69c90bbf505affa24cdad1cc2e1ef
danhtran2mind <cprmmb99@gmail.com>
2025-11-27 16:33:19 +0000

    Deploying to gh-pages from @ danhtran2mind/danhtran2mind.github.io@2555a066a801153da5d3ab61ad5cd03fec73add7 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "danhtran2mind/danhtran2mind.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
